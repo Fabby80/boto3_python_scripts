bf5df278c62a041e25d8fa9ebbe5557ed4f33d06
Fabby80 <fabian.chi@outlook.com>
2024-09-02 17:54:31 +0300

    commit

--- BOTO3/VPC/Delete/delete_internet_gateways.py
+++ BOTO3/VPC/Delete/delete_internet_gateways.py
@@ -1,11 +1,9 @@
 import boto3
 
 internet_gateway_id = 'igw-0b6b97859e7f264a8'
-vpc_id = 'vpc-03b2d3bb933416853'
 
 ec2 = boto3.client('ec2')
 
-ec2.detach_internet_gateway(
+ec2.delete_internet_gateway(
     InternetGatewayId=internet_gateway_id,
-    VpcId=vpc_id,
 )
